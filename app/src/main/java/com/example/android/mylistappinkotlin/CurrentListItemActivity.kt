dd91540d8d2df94a22dfcfa7defb6542a20682c2
Armen Sarkisyan <palanka9@mail.ru>
2021-03-07 02:39:50 +0300

    Added new Activity

---
+++ app/src/main/java/com/example/android/mylistappinkotlin/CurrentListItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.mylistappinkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentListItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_list_item)
+    }
+}
